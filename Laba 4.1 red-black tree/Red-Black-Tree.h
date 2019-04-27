21dd33d2cb3664d3a6418fc6c79c6d943a8748db
AnastasiyaNovikovaa <43859713+AnastasiyaNovikovaa@users.noreply.github.com>
2019-04-27 13:42:11 +0300

    change for lab 4.3

--- Laba 4.1 red-black tree/Red-Black-Tree.h
+++ Laba 4.1 red-black tree/Red-Black-Tree.h
@@ -5,15 +5,37 @@
 using namespace std;
 
 
+template <class item> class stack
+{
+	item *Stack; int size_;
+public:
+	stack(int maxt) : Stack(new item[maxt]), size_(0) {};
+	~stack();
+	bool empty() const { return size_ == 0; }
+	int size() const { return size_; }
+	void push(item & item) { Stack[size_++] = item; }
+	item top() const { return Stack[size_ - 1]; }
+	void pop() { if (size_) --size_; }
+};
+
+
+
+// функция деструктора Стека
+template <class item>
+stack<item>::~stack()
+{
+	free(Stack); // удаляем стек
+}
+
 template <class item> class queue
 {
-	item* Queue; int head, tail, size_, Max;
+	item *Queue; int head, tail, size_, Max;
 public:
 	queue(int maxQ) : head(0), tail(0), Max(maxQ), size_(0), Queue(new item[maxQ + 1]) {}
 	~queue();
 	bool empty() const { return (head % Max) == tail; }
 	int size() const { return size_; }
-	void push(item& item) { Queue[tail++] = item; tail %= Max; ++size_; }
+	void push(item & item) { Queue[tail++] = item; tail %= Max; ++size_; }
 	item front() const { return Queue[head % Max]; }
 	void pop() { head++; head %= Max; --size_; }
 };
@@ -24,39 +46,45 @@ queue<item>::~queue()
 	free(Queue);
 }
 
-template <typename T, typename T2 >
-class Map
+template <typename T >
+class map //на map
 {
-	private:
+private:
 
 	class node
 	{
 
 	public:
-		node(T key, T2 data, node * left = nullptr, node * right = nullptr) {
-			this->data = data;
-			this->left = left;
-			this->right = right;
+		node(T key,  node * next1 = nullptr, node * next2 = nullptr) {
+			this->next_left = next1;
+			this->next_right = next2;
 			this->key = key;
 			this->parent = nullptr;
-			this->color = 'r';
+			this->color = 1;
+			this->frequancy = 0;
+			this->code = {};
 
 		};
-		char color;
-		T key, data;
-		node * right;
-		node *left;
-		node *parent;
+		bool color;
+		size_t frequancy;
+		string code;
+		T key;
+		node * next_right, *next_left, *parent;
+		//unsigned int height;
+		void Del();
+		~node();
+
 	};
 
 	node * root;
 	size_t size;
-	
-  public:
-		Map();
-		~Map();
-	
-	class TreeIterator : public Iterator<T, T2> {
+public:
+	map();
+	~map();
+
+	void set_code(string str, char data) { find(data)->code = str; };
+	string get_code(char data) { return find(data)->code; };
+	class TreeIterator : public Iterator<T> {
 	protected:
 		node *current;
 		TreeIterator(node* root) : current(root) { }
@@ -64,51 +92,62 @@ class Map
 	public:
 		bool operator==(const nullptr_t) const override { return current == nullptr; }
 		bool operator!=(const nullptr_t) const override { return !operator==(nullptr); }
-		T2 operator*() const override { return current->data; }
-		T current_key() { return current->key; }
-		T2 current_value() { return current->data; }
-		char current_color() { return current->color; }
-
+		size_t operator*() const override
+		{ return current->frequancy; }
+		T current_key() 
+		{ return current->key; }
+		bool current_color() { return current->color; }
+		T current_code()
+		{
+			return current->code;
+		}
+		node *get_cur() { return current; }
 	};
+
+
 
 	class BftIterator : public TreeIterator {
 	private:
 		queue<node*> nodes;
 	public:
 		BftIterator(node *root, size_t size) : TreeIterator(root), nodes(size) {}
-		void operator++(T) override;
+		void operator++(int) override;
 	};
-private:
-	void insertfix(node *t);      //supporting function for insert
+
+	//mode* remove(int);
+	void reset_list();
+
+	node *get_uncle(node*);
+	node* get_grandparent(node *);
+	node* get_sibling(node *);
+	node *get_successor(node *);
+
+	//void add_first(node*); перенести в приват
+	void add_first(T);
+	void remove(T);
+	void delfix(node*);
+	node * find(T);
+
+	List <T> get_keys();
+	T get_colors();
+	void get_value();
+	void clear();
+	void insert(T);
+	void insertfix(node *&t);
 	void leftrotate(node *);
 	void rightrotate(node *);
-	void reset_list();
-	node *get_leaf(node *);       //supporting function for remove
-	void add_first(node*);        //adding the first element to the tree
-	void delfix(node*);           //supporting function for remove, balance
-    
-  public:
-	void get_keys();
-	void get_value();
-	void clear();                 //clearing the tree
-	void insert(T, T2);           //insert element in the tree
-	void remove(T);               //removing element by key
-	int get_size();               //getting size of the tree
-	int find(T);                 //finding element by key
-	bool Find(T key, T2*& value);
 	BftIterator create_bft_iterator() { return BftIterator(root, size); }
-	
-	
 };
 
 
-template<typename T, typename T2>
-void Map<T, T2>::BftIterator::operator++(T)
+
+template<typename T>
+void map<T>::BftIterator::operator++(int)
 {
-	if (this->current->left != nullptr)
-		nodes.push(this->current->left);
-	if (this->current->right != nullptr)
-		nodes.push(this->current->right);
+	if (this->current->next_left != nullptr)
+		nodes.push(this->current->next_left);
+	if (this->current->next_right != nullptr)
+		nodes.push(this->current->next_right);
 	if (nodes.size() > 0) {
 		this->current = nodes.front();
 		nodes.pop();
@@ -116,3 +155,4 @@ void Map<T, T2>::BftIterator::operator++(T)
 	else
 		this->current = nullptr;
 }
+
